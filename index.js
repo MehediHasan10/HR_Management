3440bd03777790ee1ccec965c53532aaa49f12cf
Mehedi Hasan <MehediHasan10>
2020-10-06 10:09:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+//reference of express module
+const app = express();
+
+//parsing the json data
+app.use(express.json());
+
+//mongo config and connection
+const url = "mongodb+srv://el06:test1234@cluster0.a9rlb.mongodb.net/hrdb";
+mongoose.connect(url, { 
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+}).catch((err) => console.log(err)); //Handles initial connection error
+
+const db = mongoose.connection;
+db.on('error', () => {
+    console.log('> Error occurred from database...');
+});
+db.once('open', () => {
+    console.log('> Database is connected successfully...');
+});
+
+//view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//testing the server
+app.get('/', (req, res) => {
+    // res.send("all ok");
+    res.render("pages/dashboard");
+})
+
+//bring all routes
+const hrRouter = require('./routes/hrDb');
+//routes handler
+// app.use('/', hrRouter);
+
+//set up the public folder
+app.use(express.static('./public'));
+
+//server listener
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log("Server listening on port " + port + "..."));
